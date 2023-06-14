8c093bb5764eadc0115368fad659b753e5d86fa6
Min Seo <seominwoong1@gmail.com>
2023-06-14 18:06:35 +0100

    fix - fixed products page

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,7 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
 <!-- FOOTER -->
+<link rel="stylesheet" href="stylesheet/footer.css">
 <div class="footer">
   <img src="images/productspage/SafeSystem Logo.png" />
 </div>
--- application/views/topnav.php
+++ application/views/topnav.php
@@ -128,4 +128,4 @@
     </ul>
   </div>
 
-  <script src="js/aboutus.js"></script>
+  <script src="js/topnav.js"></script>

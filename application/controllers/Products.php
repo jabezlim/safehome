390a69f5f9c7b35883b93c216c9a84c8d24ac418
Min Seo <seominwoong1@gmail.com>
2023-06-14 16:11:37 +0100

    feat - latest version from previous repo

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -1,10 +1,12 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Products extends CI_Controller {
+class Products extends CI_Controller
+{
 	public function index()
 	{
 		$this->load->view('header');
 		$this->load->view('products');
+		$this->load->view('footer');
 	}
 }

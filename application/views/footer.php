59d6a4ffd2f0a43abf757e990db2e711d5281d62
Yamire <78925053+Yamire16@users.noreply.github.com>
2023-06-15 14:05:13 +0100

    Finished Footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,27 +1,29 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
 <!-- FOOTER -->
-<link rel="stylesheet" href="stylesheet\footertest.css">
+<link rel="stylesheet" href="stylesheet\footer.css">
 <footer>
   <img src="images/productspage/SafeSystem Logo.png" />
-  <div class="Table">
-    <h1>Hyperlinks</h1>
-    <ul>
-      <li>Home</li>
-      <li>About us</li>
-      <li>Certificates</li>
-      <li>Products</li>
-    </ul>
-  </div>
-  <div class="Table">
-    <h1>Contacts</h1>
-    <ul>
-      <li>TEL 031 425 0830 4</li>
-      <li>FAX 031 299 2260</li>
-      <li>info@safesystem.co.kr</li>
-    </ul>
-  </div>
-  <hr class="footer_HR">
+  <div class="Tables">
+    <div class="Table">
+      <h1>Hyperlinks</h1>
+      <ul>
+        <a href="home"><li>Home</li></a>
+        <a href="about"><li>About us</li></a>
+        <a href="certificate"><li>Certificates</li></a>
+        <a href="products"><li>Products</li></a>
+      </ul>
+    </div>
+    <div class="Table">
+      <h1>Contacts</h1>
+      <ul>
+        <li>TEL 031 425 0830 4</li>
+        <li>FAX 031 299 2260</li>
+        <li>info@safesystem.co.kr</li>
+      </ul>
+    </div>
+  </div>  
+  <hr>
   <h1 class="CopyrightNote">Copyright 2013 SAFE SYSTEM ALL RIGHTS RESERVED</h1>
 </footer>
 </body>

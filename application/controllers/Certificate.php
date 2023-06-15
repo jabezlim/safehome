ae4ff6a51826b9b7750f25de346546845add8914
Yamire <78925053+Yamire16@users.noreply.github.com>
2023-06-15 15:18:14 +0100

    removed and added images to new folders

--- application/controllers/Certificate.php
+++ application/controllers/Certificate.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Certificate extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('header');
+		$this->load->view('topnav');
 		$this->load->view('certificate');
 		$this->load->view('footer');
 	}
--- application/views/contact.php
+++ application/views/contact.php
@@ -1,1 +1,36 @@
-<h1>Contact Us</h1>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+
+<link rel="stylesheet" href="stylesheet/contact.css">
+<img src="images\contactpage\ContactCover.gif  " class="ContactCover">
+<img src="images\contactpage\ContactQuote.png" class="ContactQuote">
+<main class="main">
+            <div class="container">
+              <h1>Certificate pages</h1>
+              <ul class="pages" onclick="changeButtonColor_Border(event)">
+                <li><button>Service</button></li>
+                <li><button>Service</button></li>
+                <li><button>Service</button></li>
+                <li><button>FAQ</button></li>
+                <li><button>Q&A</button></li>
+              </ul>
+            </div>
+            <div class="container_right">
+              <div class="container_right_top" >
+                <h1 hidden>Service</h1>
+                <h1 hidden>Service 2</h1>
+                <h1 hidden>Service 3</h1>
+                <h1 hidden>FAQ</h1>
+                <h1 hidden>Q&A</h1>
+              </div>
+            <div class="container_right_bottom">
+                <img src="images\contactpage\ContactImg1.gif" hidden>
+                <div></div>
+                <div></div>
+                <div>
+                    <h1>Something</h1>
+
+                </div>
+                <div></div>
+            </div>
+</main>
+<script src="js/contact.js"></script>
